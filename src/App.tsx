8488e4f11a3409b483c9f2ce153ba4e8bbf646fe
Yure Braga <yure.braga@outlook.com>
2025-08-12 21:53:31 -0300

    Primeiros passos do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TimerIcon } from "lucide-react";
+import { Heading } from "./components/Heading";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./styles/global.css";
+import "./styles/theme.css";
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Heading>
+        Olá Mundo 1
+        <button>
+          <TimerIcon />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        laborum unde magni cupiditate saepe iusto eveniet, atque praesentium
+        facilis autem, rerum molestiae, quibusdam architecto! Placeat soluta
+        totam nihil error neque!
       </p>
     </>
-  )
+  );
 }
-
-export default App
